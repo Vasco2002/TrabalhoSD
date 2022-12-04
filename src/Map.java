0066cffb480fc15e52f769edd049dcc0035d6518
GuiSSMartins <60627482+GuiSSMartins@users.noreply.github.com>
2022-12-04 01:37:23 +0000

    Adicionada função para obter os locais com trotinetes livres

--- src/Map.java
+++ src/Map.java
@@ -1,14 +1,16 @@
 package src;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Map {
 
     private int n; // Number of lines/columns of the map
     private Location[][] map;
-    // private HashMap<String, Location> utilizadoresLocal;
+    private HashMap<String, Location> usersLocation;
     private HashMap<Location, Integer> freeScooters; // value is the number of dree scooters in the given location
     private Integer reservationCode = 0; // counter of the reservation
     private Queue<Reservation> reservation; // queue with the reservation
@@ -27,7 +29,7 @@ public class Map {
                 this.map[i][j] = novoLocal;
             }
         }
-        // this.utilizadoresLocal = new HashMap<>();
+        this.usersLocation = new HashMap<>();
         this.freeScooters = new HashMap<>();
         this.reservation = new LinkedList<>();
         this.clientReservation = new HashMap<>();
@@ -37,22 +39,26 @@ public class Map {
     //     this.utilizadoresLocal.put(username, proximo);
     // }
 
-    public void realizarReserva(String username, Location l) 
+    public void rmakeReservation(String username, Location l) 
     {
         if (this.clientReservation.containsKey(username)) 
         {
-            if (!this.clientReservation.get(username)) {
+            if (!this.clientReservation.get(username) && (this.freeScooters.get(l)) > 0) { // the location needs to heve free scooters
                 Reservation reserv = new Reservation(this.reservationCode, username, l);
                 this.reservation.add(reserv);
                 this.clientReservation.put(username, true);
                 this.reservationCode++;
+                int n_scooters = this.freeScooters.get(l);
+                this.freeScooters.put(l, n_scooters-1);
             }
         }
-        else {
+        else if (this.freeScooters.get(l) > 0) {
             Reservation reserv = new Reservation(this.reservationCode, username, l);
             this.reservation.add(reserv);
             this.clientReservation.put(username, true);
             this.reservationCode++;
+            int n_scooters = this.freeScooters.get(l);
+            this.freeScooters.put(l, n_scooters-1);
         }
     }
 
@@ -67,14 +73,19 @@ public class Map {
         else return true;
     }
 
-    /*
     // Devolve uma lista com os locais com trotinetes livres 
-    public List<Local> locaisfreeScooters(Integer d) {
-        List<Local> comTrotinestesLivres = new ArrayList<>();
+    public List<Location> locationsFreeScooters(Integer d, String username) {
+        List<Location> withFreeScooters = new ArrayList<>();
+        Location userLocation = this.usersLocation.get(username);
 
-        return comfreeScooters;
+        for (Location l : this.freeScooters.keySet()) {
+            if (userLocation.distance(l) > d) {
+                if (this.freeScooters.get(l) > 0) withFreeScooters.add(l);
+            }
+        }
+
+        return withFreeScooters;
     }
-    */
 
 
 }
